575de5bcc4ffbad556800e47e70034e8ad2f4bc4
i-zanis <55964867+i-zanis@users.noreply.github.com>
2020-12-23 02:06:15 +0000

    FilePath class to store file paths

---
+++ src/main/java/org/uwl/cs/FilePaths.java
@@ -0,0 +1,10 @@
+package org.uwl.cs;
+
+public class FilePaths {
+    public static String LOGIN = "FXML/registrationpage.fxml";
+    public static String REGISTRATION = "FXML/registrationpage.fxml";
+    public static String MAINMENU     = "FXML/mainmenu.fxml";
+    public static String FAILED_AUTHENTICATION       = "FXML/authenticationFailedPage.fxml";
+    public static String CSS     = "/PackageFiles/CSS/style.css";
+    public static String WINDOW_ICON    = "/PackageFiles/Media/dragonpic.png";
+}
--- src/main/java/org/uwl/cs/Main.java
+++ src/main/java/org/uwl/cs/Main.java
@@ -4,22 +4,35 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
-import java.io.IOException;
+import static org.uwl.cs.FilePaths.LOGIN;
 
 public class Main extends Application {
 
     public static void main(String[] args) {
         launch(args);
     }
-
     @Override
-    public void start(Stage primaryStage) throws IOException {
-        primaryStage.setTitle("Hello World");
-        Pane pane = new Pane();
-        primaryStage.setScene(new Scene(pane));
+    public void start(Stage primaryStage) throws Exception {
+        // sets the initial FXML file to be loaded
+        Parent root = FXMLLoader.load(getClass().getResource(LOGIN));
+        // Create a scene and set the initial(root) FXML
+        Scene logInScene = new Scene(root);
+        // Clear any previous CSS to avoid inconsistencies
+        logInScene.getStylesheets().clear();
+        // Add CSS from separate Style Sheet
+       // logInScene.getStylesheets().add(styleCSS);
+        // Sets the title of the window
+        primaryStage.setTitle("Bank of West London");
+        // Sets the icon of the window in the Taskbar and top window bar
+        //primaryStage.getIcons().add(new Image(dragonPic));
+        // Sets the Scene to the Stage
+        primaryStage.setScene(logInScene);
+        // Shows the Stage
         primaryStage.show();
+        // makes the window not resizable
+        primaryStage.setResizable(false);
     }
 }
+
