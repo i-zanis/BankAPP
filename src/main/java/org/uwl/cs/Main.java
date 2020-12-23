a06a97b3941fd20916bb215ddcdd60c8c41cc6f6
i-zanis <55964867+i-zanis@users.noreply.github.com>
2020-12-23 01:02:41 +0000

    Git corruption of files, reupload

---
+++ src/main/java/org/uwl/cs/Main.java
@@ -0,0 +1,25 @@
+package org.uwl.cs;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        primaryStage.setTitle("Hello World");
+        Pane pane = new Pane();
+        primaryStage.setScene(new Scene(pane));
+        primaryStage.show();
+    }
+}
